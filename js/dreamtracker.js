3641d633e6d64416c7150b59b69879824f607843
anamindful <anamindful@gmail.com>
2024-05-29 19:46:10 -0500

    adding the js code

--- js/dreamtracker.js
+++ js/dreamtracker.js
@@ -1,4 +1,28 @@
 class DreamTracker {
+    constructor({
+        selector,
+        targetDate
+    }) {
+        this.selector = selector
+        this.targetDate = targetDate
+    }
 
+    getTimeRemaining(endtime){
+        console.log(endtime)
+    }
+
+    updateTimer({ hours, mins, secs}){
+
+    }
+
+    startTimer(){
+        const timer = this.getTimeRemaining(this.targetDate)
+    }
 }
 
+const timer1 = new DreamTracker ({
+    selector: '#clock1',
+    targetDate: new Date ('June, 25 2024 14:00:00')
+})
+
+timer1.startTimer()
